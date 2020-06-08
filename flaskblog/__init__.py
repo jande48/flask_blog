3c22c8631d6668e239c55190ca16aa1284c5bf59
Jacob Anderson <jacob.anderson10@gmail.com>
2020-06-08 07:34:23 -0500

    changed routes to blueprint structure.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,18 +6,27 @@ from flask_login import LoginManager
 from flask_mail import Mail 
 
 app=Flask(__name__)
-app.config['SECRET_KEY'] = '6c5ff70ab1a65ef44bf936b08062588f'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+mail = Mail(app)
+app.config['SECRET_KEY'] = '6c5ff70ab1a65ef44bf936b08062588f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'norely.flask.blog@gmail.com'
 app.config['MAIL_PASSWORD'] = 'qzjs1trav*'
-mail = Mail(app)
 
-from flaskblog import routes
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
+
+
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flaskblog.models import User
 from flask_login import current_user
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
@@ -23,11 +23,13 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That username is taken. Please choose another')
 
+
 class LogInForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
     password = PasswordField('Password',validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login') 
+
 
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username',
@@ -49,10 +51,6 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
 
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content',validators=[DataRequired()])
-    submit = SubmitField('Post')
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email',
